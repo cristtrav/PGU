0ede07f0b6f59a8b7fb5321a0bf545a61e4ff73a
cristhian <cristhian@cristh-notebook>
2013-12-04 23:10:08 -0300

    Agregada la función de modificar Docentes

--- PGU-war/src/java/com/pgu/manbeans/academico/DocenteManBean.java
+++ PGU-war/src/java/com/pgu/manbeans/academico/DocenteManBean.java
@@ -17,6 +17,7 @@ import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import org.primefaces.context.RequestContext;
 
 /**
  *
@@ -25,6 +26,28 @@ import javax.faces.context.FacesContext;
 @Named(value = "docenteManBean")
 @SessionScoped
 public class DocenteManBean implements Serializable {
+
+    private List<Docente> selDocente; //Lista para guardar los elementos seleccionados de la tabla
+
+    public List<Docente> getSelDocente() {
+        return selDocente;
+    }
+
+    public void setSelDocente(List<Docente> selDocente) {
+        this.selDocente = selDocente;
+    }
+
+
+    private Docente selSimpDocente; //Objeto docente para almacenar la seleccion de la tabla
+
+    public Docente getSelSimpDocente() {
+        return selSimpDocente;
+    }
+
+    public void setSelSimpDocente(Docente selSimpDocente) {
+        this.selSimpDocente = selSimpDocente;
+    }
+
     
     @EJB
     private SesUtils sesUtils;
@@ -78,10 +101,28 @@ public class DocenteManBean implements Serializable {
         this.docenteFacade.create(tmpDocente);
         this.lstDocentes.add(tmpDocente);
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Docente Agregado",""));
+        this.tmpDocente=new Docente();
     }
     
     public void recargarDocente(){
             this.sesUtils.refrescarEntidad(lstDocentes);
+    }
+    
+    public void mostrarDetalleDoc(){
+        if(this.selDocente.size()==1){
+            this.selSimpDocente=this.selDocente.get(0);
+            RequestContext.getCurrentInstance().update("formDetalleDocente");
+            RequestContext.getCurrentInstance().execute("dlgDetalleDocente.show()");
+        }
+    }
+    
+    public void modificarDocente(){
+        this.docenteFacade.edit(this.selSimpDocente);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,"Modificado correctamente",""));
+    }
+    
+    public void nuevoDocente(){
+        this.tmpDocente=new Docente();
     }
     
 }
